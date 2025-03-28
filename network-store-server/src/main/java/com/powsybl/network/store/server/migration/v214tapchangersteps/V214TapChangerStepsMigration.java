c0b265cd123ffe1ec43853807cbfce654014217f
Etienne LESOT <etienne.lesot@rte-france.com>
2025-03-28 16:48:10 +0100

    fix merge
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- network-store-server/src/main/java/com/powsybl/network/store/server/migration/v214tapchangersteps/V214TapChangerStepsMigration.java
+++ network-store-server/src/main/java/com/powsybl/network/store/server/migration/v214tapchangersteps/V214TapChangerStepsMigration.java
@@ -12,6 +12,7 @@ import com.powsybl.network.store.model.ResourceType;
 import com.powsybl.network.store.model.TapChangerStepAttributes;
 import com.powsybl.network.store.model.TapChangerType;
 import com.powsybl.network.store.server.ExtensionHandler;
+import com.powsybl.network.store.server.LimitsHandler;
 import com.powsybl.network.store.server.Mappings;
 import com.powsybl.network.store.server.NetworkStoreRepository;
 import com.powsybl.network.store.server.dto.OwnerInfo;
@@ -50,7 +51,7 @@ public final class V214TapChangerStepsMigration implements CustomTaskChange {
     public void init(Database database) {
         DataSource dataSource = new SingleConnectionDataSource(((JdbcConnection) database.getConnection()).getUnderlyingConnection(), true);
         ObjectMapper mapper = new ObjectMapper();
-        this.repository = new NetworkStoreRepository(dataSource, mapper, new Mappings(), new ExtensionHandler(mapper));
+        this.repository = new NetworkStoreRepository(dataSource, mapper, new Mappings(), new ExtensionHandler(mapper), new LimitsHandler(dataSource, mapper));
     }
 
     @Override
