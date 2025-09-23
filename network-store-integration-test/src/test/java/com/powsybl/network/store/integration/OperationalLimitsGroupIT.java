8ddca7e428565d184ce959f93e33e14d499a5c95
Etienne LESOT <etienne.lesot@rte-france.com>
2025-09-23 12:57:59 +0200

    add integration test
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

---
+++ network-store-integration-test/src/test/java/com/powsybl/network/store/integration/OperationalLimitsGroupIT.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.network.store.integration;
+
+import com.powsybl.iidm.network.Line;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.OperationalLimitsGroup;
+import com.powsybl.iidm.network.test.FourSubstationsNodeBreakerFactory;
+import com.powsybl.network.store.client.NetworkStoreService;
+import com.powsybl.network.store.server.NetworkStoreApplication;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import java.util.List;
+import java.util.UUID;
+
+import static com.powsybl.network.store.integration.TestUtils.createNetworkStoreService;
+
+/**
+ * @author Etienne Lesot <etienne.lesot at rte-france.com>
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextHierarchy({@ContextConfiguration(classes = {NetworkStoreApplication.class, NetworkStoreService.class})})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class OperationalLimitsGroupIT {
+    @DynamicPropertySource
+    private static void makeTestDbSuffix(DynamicPropertyRegistry registry) {
+        UUID uuid = UUID.randomUUID();
+        registry.add("testDbSuffix", () -> uuid);
+    }
+
+    @LocalServerPort
+    private int randomServerPort;
+
+    @Test
+    void testDeleteOperationalLimitsGroup() {
+        try (NetworkStoreService service = createNetworkStoreService(randomServerPort)) {
+            Network network = FourSubstationsNodeBreakerFactory.create(service.getNetworkFactory());
+            Line lineS3S4 = network.getLine("LINE_S3S4");
+            lineS3S4.newOperationalLimitsGroup2("TEST")
+                    .newCurrentLimits()
+                    .setPermanentLimit(300)
+                    .beginTemporaryLimit()
+                    .setName("IST")
+                    .setValue(1000)
+                    .setFictitious(false)
+                    .setAcceptableDuration(Integer.MAX_VALUE)
+                    .endTemporaryLimit()
+                    .beginTemporaryLimit()
+                    .setName("LD51")
+                    .setValue(Double.MAX_VALUE)
+                    .setAcceptableDuration(600)
+                    .endTemporaryLimit()
+                    .add();
+            lineS3S4.newOperationalLimitsGroup2("TEST2")
+                    .newCurrentLimits()
+                    .setPermanentLimit(450)
+                    .beginTemporaryLimit()
+                    .setName("IST")
+                    .setValue(1100)
+                    .setFictitious(false)
+                    .setAcceptableDuration(Integer.MAX_VALUE)
+                    .endTemporaryLimit()
+                    .beginTemporaryLimit()
+                    .setName("LD61")
+                    .setValue(Double.MAX_VALUE)
+                    .setAcceptableDuration(1200)
+                    .endTemporaryLimit()
+                    .add();
+            service.flush(network);
+        }
+
+        try (NetworkStoreService service = createNetworkStoreService(randomServerPort)) {
+            Network network = service.getNetwork(service.getNetworkIds().keySet().iterator().next());
+            Line lineS3S4 = network.getLine("LINE_S3S4");
+            // check olg are created
+            Assertions.assertTrue(lineS3S4.getSelectedOperationalLimitsGroup2().isPresent());
+            Assertions.assertEquals("DEFAULT", lineS3S4.getSelectedOperationalLimitsGroup2().get().getId());
+            List<OperationalLimitsGroup> operationalLimitsGroupList = lineS3S4.getOperationalLimitsGroups2().stream().toList();
+            Assertions.assertEquals(3, operationalLimitsGroupList.size());
+
+            // remove them
+            lineS3S4.removeOperationalLimitsGroup2("TEST2");
+            lineS3S4.removeOperationalLimitsGroup2("TEST");
+            service.flush(network);
+        }
+
+        try (NetworkStoreService service = createNetworkStoreService(randomServerPort)) {
+            Network network = service.getNetwork(service.getNetworkIds().keySet().iterator().next());
+            Line lineS3S4 = network.getLine("LINE_S3S4");
+            Assertions.assertTrue(lineS3S4.getOperationalLimitsGroup2("TEST2").isEmpty());
+            Assertions.assertTrue(lineS3S4.getOperationalLimitsGroup2("TEST").isEmpty());
+        }
+    }
+}
